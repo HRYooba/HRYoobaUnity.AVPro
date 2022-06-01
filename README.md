03cc82ef0425bac510077a034981c28a9728ec8d
1-10 Ooba RazerBlade <ooba@1-10.com>
2022-06-02 06:47:38 +0900

    [add] first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # HRYoobaUnityLibrary.AVPro
+## 1.インストール
+`https://github.com/HRYooba/HRYoobaUnityLibrary.AVPro.git`
+
+## 2.依存ライブラリ
+以下のスコープを追加してください。
+```json
+  "scopedRegistries": [
+    {
+      "name": "package.openupm.com",
+      "url": "https://package.openupm.com",
+      "scopes": [
+        "com.openupm",
+        "com.neuecc.unirx",
+        "com.cysharp"
+      ]
+    }
+  ]
+```
+
---
+++ Runtime/MediaPlayerExtention.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+using Cysharp.Threading.Tasks;
+using UniRx;
+
+namespace HRYooba.Library
+{
+    /// <summary>
+    /// AVProのMediaPlayerの拡張
+    /// </summary>
+    public static class MediaPlayerExtention
+    {
+        /// <summary>
+        /// OpenMediaの非同期
+        /// </summary>
+        /// <param name="mediaPlayer">self</param>
+        /// <param name="path">パス</param>
+        /// <param name="cancellationToken">CancellationToken</param>
+        /// <param name="pathType">パスタイプ</param>
+        /// <param name="autoPlay">自動再生するか</param>
+        /// <param name="timeout">タイムアウト処理の秒数</param>
+        /// <returns></returns>
+        public static async UniTask OpenMediaAsync(this MediaPlayer mediaPlayer, string path, CancellationToken cancellationToken, MediaPathType pathType = MediaPathType.AbsolutePathOrURL, bool autoPlay = false, float timeout = 5.0f)
+        {
+            try
+            {
+                // Media open
+                mediaPlayer.OpenMedia(pathType, path, autoPlay);
+
+                // Mediaが開けるまで待機
+                await UniTask.WaitWhile(() => !mediaPlayer.MediaOpened).Timeout(TimeSpan.FromSeconds(timeout));
+
+                // Create DebugLog Task
+                var showDebugLogAsync = UniTask.Create(async () =>
+                {
+                    await UniTask.WaitWhile(() => mediaPlayer.Info.GetVideoFrameRate() == 0, cancellationToken: cancellationToken);
+                    Debug.Log($"Opened Media: {path} {mediaPlayer.Info.GetVideoWidth()}x{mediaPlayer.Info.GetVideoHeight()} FPS{mediaPlayer.Info.GetVideoFrameRate().ToString("F2")} Duration{mediaPlayer.Info.GetDuration()}");
+                });
+
+                // デバッグログが出力終わるまでawaitで待ってもいい。その場合は await showDebugLogAsync();
+                showDebugLogAsync.Forget();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 再生終了のIObservable
+        /// </summary>
+        /// <param name="mediaPlayer">self</param>
+        /// <returns></returns>
+        public static IObservable<Unit> OnFinishedPlaying(this MediaPlayer mediaPlayer)
+        {
+            return mediaPlayer.Events.AsObservable().Where(args => args.Item2 == MediaPlayerEvent.EventType.FinishedPlaying).AsUnitObservable();
+        }
+    }
+}
